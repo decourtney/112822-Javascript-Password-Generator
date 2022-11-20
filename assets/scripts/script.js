677030ec4f9a992903e41bee0eb62c032a60cae2
decourtney <53956789+decourtney@users.noreply.github.com>
2022-11-19 18:43:47 -0600

    added remaining functions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,79 +16,124 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword()
 {
-  // Choose password length
-  let passwordLength = getPasswordLength();
-  // Choose character set - lower, upper, num, special
-  let characterSet = getCharacterSet();
-  // Default character set
-  // Generate password
-  // Display password prompt or on site
+  let passLen = getPasswordLength();
+  if (checkForNull(passLen))
+  {
+    // Cancel button was clicked    
+    return 'Password generation cancelled.\n\nClick the Generate Password button to restart.';
+  }
+
+  let charSet = getCharacterSelections();
+  if (checkForNull(charSet))
+  {
+    // Cancel button was clicked    
+    return 'Password generation cancelled.\n\nClick the Generate Password button to restart.';
+  }
+
+  let newPassword = '';
+  for (let i = 0; i < passLen; i++)
+  {
+    newPassword += charSet[Math.floor(Math.random() * charSet.length)];
+  }
+
+  return 'Your Secure Password:\n' + newPassword;
+
+
 }
 
 function getPasswordLength()
 {
   let userInput;
 
-  while (userInput < 8 || userInput > 128 || isNaN(userInput))
-  {
-    userInput = prompt("Enter password length (8 - 128)");
-    if (checkForNull(userInput))
-    {
-      // Cancel button was clicked
-      break;
-    }
-  }
-
-  return userInput;
-}
-
-function getCharacterSet()
-{
-  let userArr = [];
-
-
-  userArr.push(askUser("Include LOWERCASE characters? y/n"));
-  userArr.push(askUser("Include UPPERCASE characters? y/n"));
-  userArr.push(askUser("Include NUMBERS? y/n"));
-  userArr.push(askUser("Include SPECIAL characters? y/n"))
-
-
-
-
-  console.log(userArr)
-}
-
-function askUser(message)
-{
-  let userInput = "";
   while (true)
   {
-    userInput = prompt(message);
+    userInput = prompt("Enter password length (8 - 128)");
+
     if (checkForNull(userInput))
     {
-      // Cancel button was clicked
-      break;
+      // Cancel button was clicked    
+      return;
     }
 
-    userInput = userInput.toLowerCase().split('', 1).toString();
-
-    if (userInput === 'y')
+    if (userInput === '' || userInput < 8 || userInput > 128 || isNaN(userInput) || userInput.split('', 1) == 0)
     {
-      return true;
-    } else if (userInput === 'n')
+      window.alert('Oops, Something went wrong.\nEnter a number from 8 to 128.');
+    } else
     {
-      return false;
+      return userInput;
     }
   }
+}
+
+function getCharacterSelections()
+{
+  let userArr = [];
+  let messages = ["Include LOWERCASE characters? y/n", "Include UPPERCASE characters? y/n", "Include NUMBERS? y/n", "Include SPECIAL characters? y/n"];
+
+  while (true)
+  {
+    for (let i = 0; i < messages.length; i++)
+    {
+      while (true)
+      {
+        let userInput = prompt(messages[i]);
+
+        if (checkForNull(userInput))
+        {
+          // Cancel button was clicked
+          return;
+        } else
+
+          userArr[i] = userInput.toLowerCase().split('', 1).toString();
+
+        if (userArr[i] === 'y' || userArr[i] === 'n')
+        {
+          break;
+        } else 
+        {
+          window.alert('Oops, Something went wrong.\nType (y)es or (n)o.')
+        }
+      }
+    }
+
+    if (!userArr.includes('y'))
+    {
+      window.alert('Oops, Something went wrong.\nChoose at least one character set.');
+    } else
+    {
+      break;
+    }
+  }
+
+  return getCharSet(userArr);
+}
+
+function getCharSet(arrValues)
+{
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let numbers = '1234567890';
+  let specials = ' !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+  let charArr = ['abcdefghijklmnopqrstuvwxyz', 'abcdefghijklmnopqrstuvwxyz'.toUpperCase(), '1234567890', ' !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~']
+  let charSet = '';
+
+  for (let i = 0; i < arrValues.length; i++)
+  {
+    if (arrValues[i])
+    {
+      charSet += charArr[i];
+    }
+  }
+
+  return charSet;
 }
 
 function checkForNull(valueToCheck)
 {
-  if (valueToCheck === null)
+  if (valueToCheck == null)
   {
     return true;
   }
-  else
+  else 
   {
     return false;
   }
